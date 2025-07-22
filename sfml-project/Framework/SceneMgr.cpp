198f18f2935d913e2a49f351c0d00c87873fa0e4
서준규 <thddlwnsrb@naver.com>
2025-07-22 13:02:26 +0900

    초기 커밋

---
+++ sfml-project/Framework/SceneMgr.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "SceneMgr.h"
+#include "SceneDev1.h"
+#include "SceneDev2.h"
+
+void SceneMgr::Init()
+{
+	scenes.insert({ SceneIds::Dev1, new SceneDev1() });
+	scenes.insert({ SceneIds::Dev2, new SceneDev2() });
+
+	for (auto pair : scenes)
+	{
+		pair.second->Init();
+	}
+
+	currentScene = startScene;
+	scenes[currentScene]->Enter();
+}
+
+void SceneMgr::Release()
+{
+	for (auto pair : scenes)
+	{
+		if (pair.first == currentScene)
+		{
+			pair.second->Exit();
+		}
+		pair.second->Release();
+		delete pair.second;
+	}
+	scenes.clear();
+}
+
+void SceneMgr::ChangeScene(SceneIds id)
+{
+	nextScene = id;
+}
+
+void SceneMgr::Update(float dt)
+{
+	if (nextScene != SceneIds::None)
+	{
+		scenes[currentScene]->Exit();
+		currentScene = nextScene;
+		nextScene = SceneIds::None;
+		scenes[currentScene]->Enter();
+	}
+	scenes[currentScene]->Update(dt);
+}
+
+void SceneMgr::Draw(sf::RenderWindow& window)
+{
+	scenes[currentScene]->Draw(window);
+}
